5f116b0c127600e6621e60831d3f7731efb522e0
Baptiste Lecordier <91879838+BaptisteLECORDIER@users.noreply.github.com>
2022-10-18 22:07:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once('./inc/head.php') ?>
+
+<body class="index-body">
+    <?php require_once('./inc/navbar.php') ?>
+    <div></div>
+</body>
+
+<?php require_once('./inc/footer.php') ?>
+</html>
